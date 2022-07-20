49b1294ee9f72e205cfb56fb3c57cb3545992fd2
Pat Maddox <pat@ratiopbc.com>
2022-07-19 17:08:36 -0700

    Remove unused aliases

--- lib/gcp_error_reporting.ex
+++ lib/gcp_error_reporting.ex
@@ -7,11 +7,6 @@ defmodule GcpErrorReporting do
 
   alias GoogleApi.CloudErrorReporting.V1beta1.Connection
   alias GoogleApi.CloudErrorReporting.V1beta1.Api.Projects
-  alias GoogleApi.CloudErrorReporting.V1beta1.Model.ErrorContext
-  alias GoogleApi.CloudErrorReporting.V1beta1.Model.ReportedErrorEvent
-  alias GoogleApi.CloudErrorReporting.V1beta1.Model.ServiceContext
-  alias GoogleApi.CloudErrorReporting.V1beta1.Model.SourceLocation
-  alias GoogleApi.CloudErrorReporting.V1beta1.Model.SourceReference
 
   def report_error(message, %Reporter{goth: goth, project_id: project_id} = reporter) do
     Projects.clouderrorreporting_projects_events_report(
