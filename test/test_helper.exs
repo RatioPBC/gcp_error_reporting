084ab35347c039d446fae05713c638759dcbc985
Eric Saxby <sax@livinginthepast.org>
2022-07-26 09:51:35 -0700

    Integration test runs as :integration tag

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,5 @@
 Mox.defmock(MockGcpErrorReporting, for: GcpErrorReporting.Reporting)
 Application.put_env(:gcp_error_reporting, :impl, MockGcpErrorReporting)
 
+ExUnit.configure(exclude: [integration: true])
 ExUnit.start()
